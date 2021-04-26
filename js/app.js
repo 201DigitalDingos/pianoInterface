8f6ea991bdcec882ac6e86347f847dd93b7df0c4
Keian <KeianAnthony1@gmail.com>
2021-04-26 16:03:17 -0700

    adds custom chord feature

--- js/app.js
+++ js/app.js
@@ -1,5 +1,31 @@
 'use strict';
 
+// recording
+let recording = false;
+
+const recordButton = document.getElementById("record");
+
+// recording array
+let recordingArray = [];
+
+function localStorageUpdate() {
+  let storedNotes = localStorage.getItem(`storedKeys`);
+  if (storedNotes) {
+    recordingArray = JSON.parse(storedNotes);
+  }
+}
+
+// record button listener
+recordButton.addEventListener('click', toggleRecord);
+
+function toggleRecord() {
+  recording = !(recording);
+  console.log(recording);
+  if (recording) {
+    recordingArray = [];
+  }
+}
+
 // keys
 const buttonAS = document.getElementById("buttonAS");
 const buttonA = document.getElementById("buttonA");
@@ -17,7 +43,7 @@ const buttonG = document.getElementById("buttonG");
 // chords
 const cMajorButton = document.getElementById("CMajor");
 
-// keys
+// keys event listeners
 buttonAS.addEventListener('click', handleClickAS);
 buttonA.addEventListener('click', handleClickA);
 buttonB.addEventListener('click', handleClickB);
@@ -31,79 +57,91 @@ buttonF.addEventListener('click', handleClickF);
 buttonGS.addEventListener('click', handleClickGS);
 buttonG.addEventListener('click', handleClickG);
 
-// chords
+// chords event listeners
 cMajorButton.addEventListener('click', handleClickCMajor);
+
+// function to play notes
+function playNote(ElemID) {
+  if (recording === true) {
+    recordingArray.push(ElemID);
+    let recordedStorage = JSON.stringify(recordingArray);
+    localStorage.setItem(`storedKeys`, recordedStorage);
+  }
+  const x = document.getElementById(ElemID);
+  x.load();
+  x.play();
+}
+
+// Event listener for playback button
+const playbackButton = document.getElementById("playback");
+
+playbackButton.addEventListener('click', handlePlayback);
+
+function handlePlayback() {
+  for (let note of recordingArray) {
+    const x = document.getElementById(note);
+    x.play();
+  }
+}
 
 // AS
 function handleClickAS() {
-  const x = document.getElementById("AS3Note");
-  x.play();
+  playNote("AS3Note");
 }
 
 // A
 function handleClickA() {
-  const x = document.getElementById("A3Note");
-  x.play();
+  playNote("A3Note");
 }
 
 // B
 function handleClickB() {
-  const x = document.getElementById("B3Note");
-  x.play();
+  playNote("B3Note");
 }
 
 // CS
 function handleClickCS() {
-  const x = document.getElementById("CS3Note");
-  x.play();
+  playNote("CS3Note");
 }
 
 // C
 function handleClickC() {
-  const x = document.getElementById("C3Note");
-  x.play();
+  playNote("C3Note");
 }
 
 // DS
 function handleClickDS() {
-  const x = document.getElementById("DS3Note");
-  x.play();
+  playNote("DS3Note");
 }
 
 // D
 function handleClickD() {
-  const x = document.getElementById("D3Note");
-  x.play();
+  playNote("D3Note");
 }
 
 // E
 function handleClickE() {
-  const x = document.getElementById("E3Note");
-  x.play();
+  playNote("E3Note");
 }
 
 // FS
 function handleClickFS() {
-  const x = document.getElementById("FS3Note");
-  x.play();
+  playNote("FS3Note");
 }
 
 // F
 function handleClickF() {
-  const x = document.getElementById("F3Note");
-  x.play();
+  playNote("F3Note");
 }
 
 // GS
 function handleClickGS() {
-  const x = document.getElementById("GS3Note");
-  x.play();
+  playNote("GS3Note");
 }
 
 // G
 function handleClickG() {
-  const x = document.getElementById("G3Note");
-  x.play();
+  playNote("G3Note");
 }
 
 // C Major chord
@@ -115,3 +153,5 @@ function handleClickCMajor() {
   y.play();
   z.play();
 }
+
+localStorageUpdate();
